7ada1b6ba65feddbabe4668bb03df301f3a1cfe7
Jacob Garcia <janikgar@gmail.com>
2018-11-25 14:10:29 -0600

    add heroes fetch service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
+  providers: [HeroService]
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
   
   ngOnInit() {
+    this.getHeroes();
   }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
